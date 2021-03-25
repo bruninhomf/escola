6fe22a389e626c34e3335d6693e3fad18c38e4ee
bruninhomf <bruninhomf@msn.com>
2021-03-25 15:21:34 -0300

    Instalando e configurando plugin de exportação/importação.

---
+++ app/Imports/CoursesImport.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Course;
+use Illuminate\Support\Facades\Hash;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Excel;
+
+class CoursesImport implements ToModel, WithHeadingRow
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new Course([
+            'name'  => $row['nome'],
+            'id'    => $row['codigo']
+        ]);
+
+    }
+
+}
+
