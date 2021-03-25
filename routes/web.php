22e9b48020222ab83aeed46908c7557f25b7938d
bruninhomf <bruninhomf@msn.com>
2021-03-24 21:49:46 -0300

    Adicionando e corrigindo rotas.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +16,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return view('login');
+    });
+    Route::get('/', [HomeController::class, 'index']);
+
+    
+    Route::get('/dashboard', [HomeController::class, 'index']);
+
+
+    Route::get('/cursos', [CourseController::class, 'index']);
+    Route::get('/curso/novo', [CourseController::class, 'create']);
+    Route::post('/curso', [CourseController::class, 'store']);
+    Route::get('/curso/visualizar/{course}', [CourseController::class, 'show']);
+    Route::get('/curso/editar/{course}', [CourseController::class, 'edit']);
+    Route::post('/curso/{course}', [CourseController::class, 'update']);
+    Route::get('/curso/apagar/{course}', [CourseController::class, 'destroy']);
+
+
+    Route::get('/alunos', [StudentController::class, 'index']);
+    Route::get('/aluno/novo', [StudentController::class, 'create']);
+    Route::post('/aluno', [StudentController::class, 'store']);
+    Route::get('/aluno/visualizar/{student}', [StudentController::class, 'show']);
+    Route::get('/aluno/editar/{student}', [StudentController::class, 'edit']);
+    Route::post('/aluno/{student}', [StudentController::class, 'update']);
+    Route::get('/aluno/apagar/{student}', [StudentController::class, 'destroy']);
+});
+
+
+
