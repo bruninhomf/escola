964a328886bc3b8f48b742081158b77df0395a90
bruninhomf <bruninhomf@msn.com>
2021-03-25 15:25:05 -0300

    Criando validação de cursos.

---
+++ app/Http/Requests/CourseRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CourseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'    => 'required|max:200', 
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required'       => 'Preencha o nome do curso.',
+            'name.max'            => 'Quantidade máximo de caracteres excedido.',
+        ];
+    }
+}
